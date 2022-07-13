658dc2b41657cfa598d2e32dccab5de3ce6b5db9
llsc <p8203@qq.com>
2022-07-14 03:23:43 +0800

    test

--- api/proxy.js
+++ api/proxy.js
@@ -5,7 +5,7 @@ module.exports = (req, res) => {
   let target = ''
   // 代理目标地址
   // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
-  if (req.url.startsWith('/old')) {
+  if (req.url.startsWith('/api/old')) {
     // 这里填目标地址
     target = 'https://olforum.sanguosha.com/'
     // 创建代理对象并转发请求
