9bc861d5eebe4ca3f36862ca43cce6ab3c898a74
llsc <p8203@qq.com>
2022-07-14 03:20:18 +0800

    fix: vercel_api

--- api/proxy.js
+++ api/proxy.js
@@ -5,20 +5,6 @@ module.exports = (req, res) => {
   let target = ''
   // 代理目标地址
   // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
-  if (req.url.startsWith('/api')) {
-  // 这里填目标地址
-    target = 'https://preolforum.sanguosha.com/'
-    // 创建代理对象并转发请求
-    createProxyMiddleware({
-      target,
-      changeOrigin: true,
-      pathRewrite: {
-        // 通过路径重写，去除请求路径中的 `/api`
-        // 例如 /api/user/login 将被转发到 http://target/user/login
-        '^/api/': '/'
-      }
-    })(req, res)
-  }
   if (req.url.startsWith('/old')) {
     // 这里填目标地址
     target = 'https://olforum.sanguosha.com/'
@@ -30,6 +16,19 @@ module.exports = (req, res) => {
         // 通过路径重写，去除请求路径中的 `/api`
         // 例如 /api/user/login 将被转发到 http://target/user/login
         '^/api/old/': '/'
+      }
+    })(req, res)
+  } else if (req.url.startsWith('/api')) {
+  // 这里填目标地址
+    target = 'https://preolforum.sanguosha.com/'
+    // 创建代理对象并转发请求
+    createProxyMiddleware({
+      target,
+      changeOrigin: true,
+      pathRewrite: {
+        // 通过路径重写，去除请求路径中的 `/api`
+        // 例如 /api/user/login 将被转发到 http://target/user/login
+        '^/api/': '/'
       }
     })(req, res)
   }
