685c41d269d82b4bb9ac8f62051eb6df248b2d61
llsc <p8203@qq.com>
2023-05-12 20:14:24 +0800

    fix: WXpay订单价格差异

--- src/views/Topup.vue
+++ src/views/Topup.vue
@@ -130,9 +130,7 @@ export default {
         return
       }
       const { account, amount } = this.user
-      let yuanbao = amount
-      if (amount != 1263) yuanbao = yuanbao - 1
-      let url = `https://payment.sanguosha.com/ol/qr_pay/wechat/order/create?account=${account}&yuanbao=${yuanbao}&page_from=0&channelid=210000`
+      let url = `https://payment.sanguosha.com/ol/qr_pay/wechat/order/create?account=${account}&yuanbao=${amount}&page_from=0&channelid=210000`
       const res = await fetch(url)
       const orderData = await res.json()
       if (orderData.code == 0) {
