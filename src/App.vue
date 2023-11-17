9d03a022ce80b7dab4ad6386477ac3da7ab11e69
llsc <p8203@qq.com>
2023-11-17 17:41:50 +0800

    文本改动

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <el-menu :default-active="activeIndex" mode="horizontal" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" router>
       <el-menu-item index="/">模拟小程序签到</el-menu-item>
-      <el-menu-item index="/vote">社区投票</el-menu-item>
+      <el-menu-item index="/vote" v-if="false">社区投票</el-menu-item>
       <el-menu-item index="/topup" v-if="isShow">自定义充值</el-menu-item>
     </el-menu>
     <keep-alive>
@@ -23,7 +23,7 @@ export default {
   mounted() {
     // console.log(this.$route.path)//懒加载导致路径不对
     this.activeIndex = this.$route.path
-    window.location.hostname == 'sgssign.netlify.app' ? this.isShow = true : void 0
+    window.location.hostname.includes('netlify.app') ? this.isShow = true : void 0
   },
   methods: {
     handleSelect(routePath) {
