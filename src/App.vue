d20c5235e88dfa73c82900534726fd831e9befd0
llsc <p8203@qq.com>
2023-11-26 00:06:20 +0800

    feat: Dark Mode

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-    <el-menu :default-active="activeIndex" mode="horizontal" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" router>
+    <el-menu id="el-menu-nav" :default-active="activeIndex" mode="horizontal" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" router>
       <el-menu-item index="/">模拟小程序签到</el-menu-item>
       <el-menu-item index="/vote" v-if="false">社区投票</el-menu-item>
       <el-menu-item index="/topup" v-if="isShow">自定义充值</el-menu-item>
+      <el-switch class="darkSwitch" v-model="mode" @change="changeMode" active-icon-class="el-icon-moon" active-value="dark" active-color="#5e7987" inactive-icon-class="el-icon-sunny" inactive-value="light" inactive-color="#c0c4c3"> </el-switch>
     </el-menu>
     <keep-alive>
       <router-view></router-view>
@@ -17,17 +18,23 @@ export default {
   data() {
     return {
       activeIndex: '/',
-      isShow: false
+      isShow: false,
+      mode: 'light'
     }
   },
   mounted() {
     // console.log(this.$route.path)//懒加载导致路径不对
     this.activeIndex = this.$route.path
-    window.location.hostname.includes('netlify.app') ? this.isShow = true : void 0
+    window.location.hostname.includes('netlify.app') ? (this.isShow = true) : void 0
   },
   methods: {
     handleSelect(routePath) {
       this.activeIndex = routePath
+    },
+    changeMode() {
+      const html = document.querySelector('html')
+      // this.isDark = !this.isDark
+      this.mode == 'dark' ? html.classList.add('dark') : html.classList.remove('dark')
     }
   },
   watch: {
@@ -42,13 +49,43 @@ export default {
 </script>
 
 <style>
-html,body{
+:root {
+  color-scheme: light dark;
+  --border-color: #dcdfe6;
+  --bg-color: #ffffff;
+  --el-text-color: #606266;
+}
+
+.dark {
+  --border-color: #4c4d4f;
+  --bg-color: #141414;
+  --el-text-color: #cfd3dc;
+  --text-color: #e5eaf3;
+}
+
+html.dark {
+  color: var(--text-color);
+  background-color: var(--bg-color);
+}
+
+html,
+body {
   height: 100%;
 }
 
 .wrapper {
   margin: 20px 60px 0;
   overflow: auto;
+}
+
+#el-menu-nav {
+  padding: 0 30px;
+}
+
+.darkSwitch {
+  position: relative;
+  top: 20px;
+  left: 10px;
 }
 
 @media only screen and (max-width: 767px) {
--- src/main.js
+++ src/main.js
@@ -2,67 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 
 // import store from './store'
-import {
-  Table,
-  TableColumn,
-  Form,
-  FormItem,
-  Message,
-  Button,
-  Input,
-  Row,
-  Col,
-  Notification,
-  Alert,
-  Menu,
-  MenuItem,
-  Link,
-  MessageBox,
-  Select,
-  Option,
-  Divider,
-  Tooltip,
-  Descriptions,
-  DescriptionsItem,
-  Dialog,
-  Image,
-  Tag
-} from 'element-ui'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 import router from './router'
 // import axios from 'axios'
 
 Vue.config.productionTip = false
 // Vue.prototype.axios = axios
-Vue.component(Table.name, Table)
-Vue.component(TableColumn.name, TableColumn)
-Vue.component(Form.name, Form)
-Vue.component(FormItem.name, FormItem)
-Vue.component(Button.name, Button)
-Vue.component(Input.name, Input)
-Vue.use(Row)
-Vue.use(Col)
-Vue.use(Alert)
-Vue.use(Menu)
-Vue.use(MenuItem)
-Vue.use(Link)
-Vue.use(Select)
-Vue.use(Option)
-Vue.use(Divider)
-Vue.use(Tooltip)
-Vue.use(Descriptions)
-Vue.use(DescriptionsItem)
-Vue.use(Dialog)
-Vue.use(Image)
-Vue.use(Tag)
-Vue.prototype.$message = Message
-Vue.prototype.$notify = Notification
-Vue.prototype.$alert = MessageBox.alert
-Vue.prototype.$confirm = MessageBox.confirm
-
+Vue.use(ElementUI)
 
 new Vue({
   router,
   // store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
